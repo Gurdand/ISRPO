b4c4612b070315d5a8faf1d796a2a4b4cac568fa
Gurdand <zmei83666@gmail.com>
2020-05-25 08:59:14 +0300

    lab_2

---
+++ lab_2/Coffee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    class Coffee
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        private int price;
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+        }
+
+        public Coffee(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
---
+++ lab_2/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Dictionary<string, object> menu = new Dictionary<string, object>();
+
+        private Coffee coffee;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            menu.Add("Американо", new Coffee("Американо", 20));
+            menu.Add("Капучино", new Coffee("Капучино", 30));
+            menu.Add("Эспрессо", new Coffee("Эспрессо", 25));
+            menu.Add("Какао", new Coffee("Какао", 15));
+
+        }
+
+        private void selectCoffee(string coffeeTipe) 
+        {
+            Coffee coffee = menu[coffeeTipe] as Coffee;
+        }
+
+        private void rbCoffee_Click(object sender, RoutedEventArgs e)
+        {
+            string coffeeTipe = (sender as RadioButton).Content as string;
+            this.coffee = menu[coffeeTipe] as Coffee;
+
+            lPrice.Content = coffee.Price;
+
+            CheckSum();
+        }
+
+        private void BEnterMoney_Click(object sender, RoutedEventArgs e)
+        {
+            lMoney.Content = Convert.ToInt32(lMoney.Content) + Convert.ToInt32(tbMoney.Text);
+
+            CheckSum();
+        }
+
+        private void CheckSum()
+        {
+            int price = Convert.ToInt32(lPrice.Content);
+            int sum = Convert.ToInt32(lMoney.Content);
+
+            if (sum >= price)
+            {
+                lChange.Content = sum - price;
+                bOk.IsEnabled = true;
+            }
+            else
+            {
+                lChange.Content = 0;
+                bOk.IsEnabled = false;
+            }
+        }
+
+        private void BOk(object sender, RoutedEventArgs e)
+        {
+            string str = "Напиток " + coffee.Name;
+
+            if (cbSugar.IsChecked == true)
+            {
+                str += " с сахаром";
+
+                if (cbMilk.IsChecked == true)
+                {
+                    str += " и молоком";
+                }
+            }
+            else if (cbMilk.IsChecked == true)
+            {
+                str += " с молоком";
+            }
+
+            str += " готов!\n Сдача: " + lChange.Content;
+            lChange.Content = 0;
+            lMoney.Content = 0;
+
+            MessageBox.Show(str);
+        }
+    }
+}
